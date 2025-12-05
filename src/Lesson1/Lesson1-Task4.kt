ea9d40349527689eef651299c2c0d0ce5c5f7f85
Kriss <krisskitsune@gmail.com>
2025-12-05 15:06:59 +0300

    KSL-1-4 about type

---
+++ src/Lesson1/Lesson1-Task4.kt
@@ -0,0 +1,16 @@
+package Lesson1
+
+fun main(){
+    val length:Long = 40_868_600_000
+    val age:Byte = 27
+    val partOfDay:Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear:Float = 2.0547945205479453E-4F
+    val apogee:Int = 327000
+    println(length)
+    println(age)
+    println(partOfYear)
+    println(partOfDay)
+    println(seconds)
+    println(apogee)
+}
