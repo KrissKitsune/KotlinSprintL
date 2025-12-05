677d312be66b60911498f4918dd1a69124e3b2bd
Kriss <krisskitsune@gmail.com>
2025-12-05 13:10:29 +0300

    KSL-1-1 tsk

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val webOrder:Int = readln().toInt()
-    val reviewOfTheOrder =readln()
+    val reviewOfTheOrder: String =readln()
     println("Отзыв к заказу №$webOrder: $reviewOfTheOrder")
 }
