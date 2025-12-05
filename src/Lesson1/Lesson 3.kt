b582019e952c960248c12a1c75020992665314b3
Kriss <krisskitsune@gmail.com>
2025-12-05 14:09:24 +0300

    KSL-1-3 about space

--- src/Lesson1/Lesson 3.kt
+++ src/Lesson1/Lesson 3.kt
@@ -1,5 +1,11 @@
 package Lesson1
-
 fun main() {
-    println("ds")
+    val year=1961
+    val hour= 9
+    val minute= 7
+    println(year)
+    println(hour)
+    println(minute)
+    //println("${year}\n ${hour}\n ${minute}\n")
+
 }
