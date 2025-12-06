956372e1212d6f14318170301cfbfde27720f349
Kriss <krisskitsune@gmail.com>
2025-12-06 14:10:04 +0300

    KSL-1-3 Add landing time

--- src/Lesson1/Lesson 3.kt
+++ src/Lesson1/Lesson 3.kt
@@ -1,19 +1,14 @@
 package Lesson1
-fun main() {
-    val year=1961
-    val hour= 10
-    val minute= 55
-    //вариант первый вывода
-    print("$year ")
-    print("$hour:")
-    println(minute)
-    //вариант второй вывода
-    print(year)
-    print(" ")
-    print(hour)
-    print(":")
-    println(minute)
-    //вариант третий вывода
-    println("$year $hour:$minute")
 
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(String.format("Время взлета: %4d %02d:%02d", year, hour, minute))
+
+    hour = 10
+    minute = 55
+
+    println(String.format("Время посадки: %4d %02d:%02d", year, hour, minute))
 }
