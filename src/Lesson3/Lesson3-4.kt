7b1a64ad3d1909efb7d0228d50c79b83a9c46ebf
Kriss <krisskitsune@gmail.com>
2025-12-06 20:12:56 +0300

    KS 3-4 tsk about chess

---
+++ src/Lesson3/Lesson3-4.kt
@@ -0,0 +1,13 @@
+package Lesson3
+
+fun main(){
+    var from = "Е2"
+    var to = "Е4"
+    var step = 1
+    println("[${from}-${to};${step}]")
+
+    from = "D2"
+    to = "D3"
+    step = 2
+    println("[${from}-${to};${step}]")
+}
