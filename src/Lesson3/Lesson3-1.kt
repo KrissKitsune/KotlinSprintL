319328ec46dcbe3e2e79ae212070195a2959fce6
Kriss <krisskitsune@gmail.com>
2025-12-06 19:53:25 +0300

    KS 3-1 tsk about user and time of day

---
+++ src/Lesson3/Lesson3-1.kt
@@ -0,0 +1,11 @@
+package Lesson3
+
+fun main() {
+
+    val name = "Пользователь"
+    var time = "Доброе утро!"
+    println("$time $name, отличного настроения!")
+
+    time = "Добрый вечер!"
+    println("$time $name, отличного настроения!")
+}
