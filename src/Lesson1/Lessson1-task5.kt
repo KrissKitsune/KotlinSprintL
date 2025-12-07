054894f01693c1ee111775519e42af2497a59682
Kriss <krisskitsune@gmail.com>
2025-12-07 17:42:02 +0300

    KS-1-5 Добавила доп.переменную, обозначающую кратность времени.

--- src/Lesson1/Lessson1-task5.kt
+++ src/Lesson1/Lessson1-task5.kt
@@ -1,10 +1,10 @@
 package Lesson1
-
+const val MULTIPLICITY_OF_TIME = 60
 fun main() {
-    val seconds = 6480
-    val minutes = seconds / 60 % 60
-    val hours = seconds / 3600
-    val sec = seconds % 60
+    val timeInSpace = 6480
+    val minutes = timeInSpace / MULTIPLICITY_OF_TIME % MULTIPLICITY_OF_TIME
+    val hours = timeInSpace / (MULTIPLICITY_OF_TIME*MULTIPLICITY_OF_TIME)
+    val sec = timeInSpace % MULTIPLICITY_OF_TIME
     val formatTime = String.format("%02d:%02d:%02d", hours, minutes, sec)
     println("Время, проведенное Гагариным в космосе: $formatTime")
 }
