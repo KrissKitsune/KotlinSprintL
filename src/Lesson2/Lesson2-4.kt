c04a1a2bae70b161b5d0a467c1795f3490768a22
Kriss <krisskitsune@gmail.com>
2025-12-08 12:36:54 +0300

    KS-2-4 add extra const for calculation

--- src/Lesson2/Lesson2-4.kt
+++ src/Lesson2/Lesson2-4.kt
@@ -1,12 +1,14 @@
 package Lesson2
 
-fun main(){
+const val PROCENT = 100
+
+fun main() {
     val iron = 11.0
     val crystal = 7.0
     val buff = 20
     println("До действия бафа добыто $iron железа и $crystal кристаллов. Бафф составлет $buff%.")
 
-    val ironBonus = ((iron/100)*buff).toInt()
-    val crystalBonus = ((crystal/100)*buff).toInt()
+    val ironBonus = ((iron / PROCENT) * buff).toInt()
+    val crystalBonus = ((crystal / PROCENT) * buff).toInt()
     println("С баффом вы получите сверху этого:\n$ironBonus железа,\n$crystalBonus кристаллов")
 }
