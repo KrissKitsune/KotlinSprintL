4464fb42105f6b0619dfc4a6f155f7e3de31b20a
Kriss <krisskitsune@gmail.com>
2025-12-08 19:28:05 +0300

    KS-3-1 fix variables names and delete extra line

--- src/Lesson3/Lesson3-1.kt
+++ src/Lesson3/Lesson3-1.kt
@@ -1,11 +1,10 @@
 package Lesson3
 
 fun main() {
-
     val nameUser = "Пользователь"
-    var timeOfDay = "Доброе утро!"
-    println("$timeOfDay $nameUser, отличного настроения!")
+    var timeOfDay = "Доброе утро"
+    println("${timeOfDay}! $nameUser, отличного настроения!")
 
-    timeOfDay = "Добрый вечер!"
-    println("$timeOfDay $nameUser, отличного настроения!")
+    timeOfDay = "Добрый вечер"
+    println("${timeOfDay}! $nameUser, отличного настроения!")
 }
