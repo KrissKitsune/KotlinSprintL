a14fde4032d82d121bc50ce9aa7f5f7b0a54a0ed
Kriss <krisskitsune@gmail.com>
2025-12-16 15:52:03 +0300

    KS-3-4 about chess. Fix extra quotes

--- src/Lesson3/Lesson3-4.kt
+++ src/Lesson3/Lesson3-4.kt
@@ -1,13 +1,13 @@
 package Lesson3
 
-fun main(){
+fun main() {
     var from = "Е2"
     var to = "Е4"
     var step = 1
-    println("[${from}-${to};${step}]")
+    println("[$from-$to;$step]")
 
     from = "D2"
     to = "D3"
     step = 2
-    println("[${from}-${to};${step}]")
+    println("[$from-$to;$step]")
 }
