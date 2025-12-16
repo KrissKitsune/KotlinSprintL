ca3e84703ac035071d0d2e692b38ef7d9e7bc25e
Kriss <krisskitsune@gmail.com>
2025-12-16 17:02:21 +0300

    KS-3-5 about chess 2.0. Add list for tsk

--- src/Lesson3/Lesson3-5.kt
+++ src/Lesson3/Lesson3-5.kt
@@ -1,9 +1,13 @@
 package Lesson3
+
 fun main() {
+
     val chess = "D2-D4;0"
-    val step = chess.substring(6)
-    val from = chess.substring(0, 2)
-    val to = chess.substring(3, 5)
+
+    val turnOfPlayer = chess.split("-", ";")
+    val step = turnOfPlayer[2]
+    val from = turnOfPlayer[0]
+    val to = turnOfPlayer[1]
 
     println(
         """
