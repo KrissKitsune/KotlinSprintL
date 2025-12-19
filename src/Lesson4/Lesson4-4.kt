b7dff8d1838959e1509925179fcc5d400fa9ed3a
Kriss <krisskitsune@gmail.com>
2025-12-19 13:54:43 +0300

    KS-4-4 fix redundant variables

--- src/Lesson4/Lesson4-4.kt
+++ src/Lesson4/Lesson4-4.kt
@@ -2,17 +2,13 @@ package Lesson4
 
 fun main() {
     val exercisesDay = 5
-
-    val exercisesLegs = exercisesDay % 2 == 0
-    val exercisesHands = !(exercisesDay % 2 == 0)
-    val exercisesBack = exercisesDay % 2 == 0
-    val exercisesBelly = !(exercisesDay % 2 == 0)
+    val isDayEven = exercisesDay % 2 == 0
     println(
         """ 
          |День тренировок - $exercisesDay!
-         |Упражнения для рук:    $exercisesHands
-         |Упражнения для ног:    $exercisesLegs
-         |Упражнения для спины:  $exercisesBack
-         |Упражнения для пресса: $exercisesBelly""".trimMargin()
+         |Упражнения для рук:    $isDayEven
+         |Упражнения для ног:    ${!isDayEven}
+         |Упражнения для спины:  $isDayEven
+         |Упражнения для пресса: ${!isDayEven}""".trimMargin()
     )
 }
