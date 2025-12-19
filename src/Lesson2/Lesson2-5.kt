d48bbc92408cd744c4fb7206e62116221d2d53c0
Kriss <krisskitsune@gmail.com>
2025-12-19 11:09:26 +0300

    KS-2-5 fix variables for calculation

--- src/Lesson2/Lesson2-5.kt
+++ src/Lesson2/Lesson2-5.kt
@@ -1,10 +1,12 @@
 package Lesson2
+
 import kotlin.math.pow
-fun main(){
+
+fun main() {
     val money = 70_000
-    val proc = (16.7)/100.0
+    val proc = 16.7
     val years = 20
 
-    val bigMoney = money*((1+proc).pow(years))
-    println(String.format("%6.3f", bigMoney))
+    val bigMoney = money * ((1 + proc / 100.0).pow(years))
+    println(String.format("%1.3f", bigMoney))
 }
