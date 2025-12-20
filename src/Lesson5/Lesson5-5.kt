017fadd6e100a69a9fc0c59fb8982388ff0729bf
Kriss <krisskitsune@gmail.com>
2025-12-20 12:21:34 +0300

    KS-5-5 fix random variables

--- src/Lesson5/Lesson5-5.kt
+++ src/Lesson5/Lesson5-5.kt
@@ -1,32 +1,28 @@
 package Lesson5
 
-import kotlin.random.Random
 import kotlin.system.exitProcess
 
 fun main() {
-    val winFirstNum = (Random.Default).nextInt(43)
-    var variableNum = (Random.Default).nextInt(43)
+    val winFirstNum = (0..42).random()
+    val variableNum = (0..42).random()
     //дальше блок кода, чтобы генерировались разные числа, чтобы сверка коллекций была релевантной
     val winSecondNum = if (winFirstNum != variableNum) {
         variableNum
-    } else (Random.Default).nextInt(43)
+    } else (0..42).random()
     val winThirdNum = if ((winFirstNum != variableNum) && (winSecondNum != variableNum)) {
         variableNum
-    } else (Random.Default).nextInt(43)
+    } else (0..42).random()
 
-    val userFirstNum: Int
-    var userSecondNum: Int
-    var userThirdNum: Int
     println("Вы участвуете в лотерее! Вам нужно ввести три разных числа в диапазоне от 0 до 42!\n Ваше первое число? ")
-    userFirstNum = readln().toInt()
-    if (!(userFirstNum in 0..42)) {
+    val userFirstNum = readln().toInt()
+    if (userFirstNum !in 0..42) {
         println("Вы ввели $userFirstNum, оно не в диапазоне от 0 до 42!")
         exitProcess(1)
     }
 
     println("Число принято!\n Ваше второе число? ")
-    userSecondNum = readln().toInt()
-    if (!(userSecondNum in 0..42)) {
+    var userSecondNum = readln().toInt()
+    if (userSecondNum !in 0..42) {
         println("Вы ввели ${userSecondNum}, оно не в диапазоне от 0 до 42!")
         exitProcess(1)
     } else if (userFirstNum != userSecondNum) {
@@ -37,8 +33,8 @@ fun main() {
     }
 
     println("Ваше третье число? ")
-    userThirdNum = readln().toInt()
-    if (!(userThirdNum in 0..42)) {
+    var userThirdNum = readln().toInt()
+    if (userThirdNum !in 0..42) {
         println("Вы ввели ${userThirdNum}, оно не в диапазоне от 0 до 42!")
         exitProcess(1)
     } else if ((userThirdNum != userFirstNum) && (userThirdNum != userSecondNum)) {
@@ -47,21 +43,21 @@ fun main() {
         println("Попробуйте еще раз...")
         userThirdNum = readln().toInt()
     }
-    val listWinNums = listOf(winFirstNum, winSecondNum, winThirdNum) //сравнение списков
-
+    val listWinNum = listOf(winFirstNum, winSecondNum, winThirdNum)
     val listUsersNum = listOf(userFirstNum, userSecondNum, userThirdNum)
-    val matchesNum = listWinNums.intersect(listUsersNum)
+
+    val matchesNum = listWinNum.intersect(listUsersNum.toSet()) //сравнение списков
     //println(matchesNum) //коллекция с числами, которые совпали
     val matchesCount = matchesNum.count()
-    // println(matchesCount) //проверка счетчика элементов массива с совпадениями
+    //println(matchesCount) //проверка счетчика элементов массива с совпадениями
 
     val resultText = when (matchesCount) {
         3 -> "Вы выйграли и получаете ДЖЕКПОТ!"
         2 -> "Вы угадали два числа из трех. Вы получаете крупный приз!"
-        1 -> "Вы угадали всего одно число из трех. Вас ждет утешительный при1з"
+        1 -> "Вы угадали всего одно число из трех. Вас ждет утешительный приз"
         else -> "Вы проиграли, попробуйте в другой раз..."
     }
     println(resultText)
 
-    println("А вот числа для победы в лотерее: ${winFirstNum}, ${winSecondNum}, ${winThirdNum}") //проверка рандомных чисел для выйгрыша
+    println("А вот числа для победы в лотерее: ${winFirstNum}, ${winSecondNum}, $winThirdNum") //проверка рандомных чисел для выйгрыша
 }
