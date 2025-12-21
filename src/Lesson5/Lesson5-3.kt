a70bd0fd99a20e1cc8cc81ab02b8c7e011dd0359
Kriss <krisskitsune@gmail.com>
2025-12-21 19:37:06 +0300

    KS-5-3 add extra const variables

--- src/Lesson5/Lesson5-3.kt
+++ src/Lesson5/Lesson5-3.kt
@@ -4,6 +4,8 @@ import kotlin.system.exitProcess
 
 const val WIN_FIRST_NUM = 15
 const val WIN_SECOND_NUM = 33
+const val MIN_WIN_NUM = 0
+const val MAX_WIN_NUM = 42
 
 fun main() {
 
@@ -11,14 +13,13 @@ fun main() {
         "Вы участвуете в лотерее! Вам нужно ввести два числа в диапазоне от 0 до 42!\n Ваше первое число? "
     )
     val userFirstNum = readln().toInt()
-    if (userFirstNum !in 0..42) {
+    if (userFirstNum !in MIN_WIN_NUM..MAX_WIN_NUM) {
         println("Вы ввели ${userFirstNum}, оно не в диапазоне от 0 до 42!")
         exitProcess(1)
     }
-// добавила проверку того, чтобы пользователь вводил числа строго из диапазона
     println("Ваше второе число? ")
     val userSecondNum = readln().toInt()
-    if (userSecondNum !in 0..42) {
+    if (userSecondNum !in MIN_WIN_NUM..MAX_WIN_NUM) {
         println("Вы ввели ${userSecondNum}, оно не в диапазоне от 0 до 42!")
         exitProcess(1)
     }
